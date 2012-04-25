9e6e4a734025211edcf23aa1fbd21f37c6501ad6
Romuald Brillout <sub@brillout.com>
2012-04-25 23:14:10 +0200

    fuck comments

--- sf/_.js
+++ sf/_.js
@@ -10,9 +10,9 @@ var dateEl         = $('date');
 var contentEl      = $('content');
 var MIN_WIDTH      = 450
 
-function setSize()
+/* SIZE of time */
+function setSize(){
 //{{{
-{
   if(document.documentElement['classList'].contains('big'))
   {
     //timeEl.setTextSize(document.body.clientWidth,document.body.clientHeight);
@@ -40,15 +40,250 @@ function setSize()
     dateEl.style.fontSize = date_new_size.fontSize+'px';
   }
   timeEl.style.fontSize = time_new_size.fontSize+'px';
-}
 //}}}
+}
 window.addEventListener('resize',setSize,false);
 
+/* OPTIONS */
+var getOpt;
+(function(){
+//{{{
+ //generate options
+  getOpt=function(id)
+  //{{{
+  {
+    if(id!=='theme')
+    {
+      var theme = getOpt('theme');
+      var ret   = theme && themes[theme] && themes[theme][id];
+      if(ret) return ret;
+    }
+    var el = $(id);
+    return el.type==='text'||el.nodeName==='SELECT'?el.value:!!el.checked;
+  };
+  //}}}
+
+ var opts = [
+   {id:'theme'             ,description:'theme'           ,default_:'lobster'                         },
+   {id:'font'              ,description:'font'            ,default_:DEFAULT_FONT,negDependency:'theme'},
+   {id:'color_font'        ,description:'font color'      ,default_:DEFAULT_FCOL,negDependency:'theme'},
+   {id:'bg'                ,description:'background image',default_:DEFAULT_BG  ,negDependency:'theme'},
+   {id:'color_icon'        ,description:'icon color'      ,default_:'#c00'                            },
+   {id:'show_seconds_title',description:'seconds in title',default_:false                             },
+   {id:'show_seconds'      ,description:'seconds'         ,default_:false                             },
+   {id:'12_hour'           ,description:'12-hour'         ,default_:DEFAULT_12HOUR                    },
+   {id:'show_pm'           ,description:'am/pm'           ,default_:true ,dependency:'12_hour'        },
+   {id:'show_date'         ,description:'date'            ,default_:true                              },
+   {id:'show_week'         ,description:'week'            ,default_:false,dependency:'show_date'      },
+   {id:'font_size'         ,description:'font size'       ,default_:MIN_WIDTH.toString()              }];
+
+ var themes = {
+   'simple':{
+      'bg':'#fff',
+      'font':'Syncopate',
+      'color_font':'#333'},
+   'steel':{
+      'bg':'http://good-wallpapers.com/pictures/6357/Gray%20Comb%20Texture.jpg',
+      'font':'Syncopate',
+      'color_font':'#a00'},
+   'lobster':{
+      'bg':'#300',
+      'font':'Lobster',
+      'color_font':'#333'},
+   'digital':{
+      'bg':'black',
+      'font':'Orbitron',
+      'color_font':'#0f0'},
+   'paper':{
+      //original URL: http://wallpaper.goodfon.ru/image/209099-1920x1200.jpg
+      'bg':'http://i.imgur.com/x97za.jpg',
+      'font':'Redressed',
+      'color_font':'#111'},
+   'ocean':{
+      'bg':'http://www.hotelclubposeidon.it/grafica/background.jpg',
+      'font':'Michroma',
+      'color_font':'#aaa'},
+   'classy':{
+      'bg':'http://www.fantasy-and-art.com/wp-content/gallery/abstract-wallpapers/between_darkness_and_wonder_black_purity_hd_wallpaper.jpg',
+      'font':'Nothing You Could Do',
+      'color_font':'#00a'}};
+
+  //generate html options
+  //{{{
+  (function()
+  {
+    var optionsEl = document.getElementById('options');
+    for(var i=0;i<opts.length;i++)
+    {
+      var opt = opts[i];
+      opt.dom = document.createElement('span');
+      opt.dom.innerHTML=opt.description;
+      opt.input = document.createElement(opt.id==='font'||opt.id==='theme'?'select':'input');
+        opt.input.id   = opt.id;
+        var isCheckbox = opt.default_===false || opt.default_===true;
+        if(opt.input.nodeName==='INPUT')
+        {
+          opt.input.type = isCheckbox?'checkbox':'text';
+          if(!isCheckbox) opt.input.style.width='35px';
+        }
+        else opt.input.style.width=opt.id==='font'?'90px':'68px';
+        if(isCheckbox) opt.dom.prependChild(opt.input);
+        else           opt.dom. appendChild(opt.input);
+      optionsEl.appendChild(opt.dom);
+    }
+
+    setTimeout(function(){//gecko requires a timeout
+      $('description')  .style.opacity='0';
+      $('optionsToggle').style.opacity='0';
+    },200);
+    $('optionsToggle').onclick=function(){document.documentElement['classList']['add']('options');$('optionsToggle').style.opacity='1'};
+    document.body.onclick=function(ev) { var target=ml.getEventSource(ev); if(!ml.isChildOf(target,$('optionsToggle'))&&!ml.isChildOf(target,$('options'))){document.documentElement['classList']['remove']('options');$('optionsToggle').style.opacity=0}};
+
+    $('theme').innerHTML='<option label="custom" value="">custom</option>';
+    for(var i in themes)
+    {
+      var fop=document.createElement('option');
+      fop.innerHTML=i;
+      $('theme').appendChild(fop);
+    }
+  })();
+  //}}}
+
+  var refreshFont;
+  (function() {
+  //{{{
+    var bodyFontLoader;
+    refreshFont=function(){if(bodyFontLoader) bodyFontLoader()};
+    ml.loadScript('http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js',function()
+    {
+      function loader(fontName,callback)
+      {
+        var attempts=0;
+        (function do_()
+        {
+          window['WebFont']['load']({'google':{'families':[fontName]},
+                                     'active':callback,
+                                     'fontinactive':function(){setTimeout(do_,Math.pow(2,attempts++)*5*1000)}
+                                   });
+        })();
+      }
+      bodyFontLoader=function()
+      {
+        var fontName = getOpt('font');
+        loader(fontName,function()
+        {
+          if(fontName===getOpt('font') && document.body.style.fontFamily!==fontName)
+          {
+            document.body.style.fontFamily=fontName;
+            setSize();
+          }
+        })
+      };
+      loader('Arvo',function(){$('options').style.fontFamily='Arvo'});
+      refreshFont();
+    });
+  /*
+    var fontChanger;
+    ml.loadScript('http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js',function()
+    //{{{
+    {
+      fontChanger=function(el,fontName,callback)
+      {
+        window['WebFont']['load']({'google':{'families':[fontName]},'active':function()
+        {
+          setTimeout(function()
+          {
+            el.style.fontFamily=fontName;
+            if(callback)callback();
+          },100);
+        }});
+      };
+      setFont($('options'),'Arvo');
+      refreshFont();
+    });
+    //}}}
+    function setFont(){if(fontChanger) fontChanger.apply(null,arguments)};
+    refreshFont=function(){setFont(document.body,getOpt('font'),setSize)};
+  */
+  //}}}
+  })();
+
+  //refresh options onchange
+  //{{{
+  (function()
+  {
+    function setOptVisibility()
+    {
+      for(var i=0;i<opts.length;i++)
+      {
+        var opt = opts[i];
+        var toHide=opt.dependency && !getOpt(opt.dependency) || opt.negDependency && getOpt(opt.negDependency);
+          opt.dom.style.width     =toHide?'0px'   :'auto';
+          opt.dom.style.height    =toHide?'0px'   :'auto';
+          opt.dom.style.visibility=toHide?'hidden':'visible';
+          opt.dom.style.position  =toHide?'absolute':'';
+          opt.dom.style.zIndex    =toHide?'-1':'';
+      }
+    }
+    function colorChangeListener(){document.documentElement.style.color=getOpt('color_font')}
+    function themeChangeListener(){colorChangeListener();refreshFont();bgChanger(getOpt('bg'));setOptVisibility()}
+
+    var bgChanger;
+    for(var i=0;i<opts.length;i++)
+    {
+      var opt = opts[i];
+      if(opt.id==='bg'){
+        bgChanger=ml.htmlBackground(opt.input.id,opt.dom,opt.default_);
+      }
+      else
+      {
+        var changeListener;
+        if(opt.id==='color_font') changeListener=colorChangeListener;
+        else if(opt.id==='theme') changeListener=themeChangeListener;
+        else if(opt.id==='font')  changeListener=refreshFont;
+        else                      changeListener=function(){domBeat(true);setOptVisibility()};
+        ml.persistantInput(opt.id,changeListener,opt.default_,0,true);
+      }
+    }
+    themeChangeListener();
+  })();
+  //}}}
+
+  //fullscreen
+  //{{{
+  (function()
+  {
+    function bigger()
+    //{{{
+    {
+      //document.body.webkitRequestFullScreen();
+      //timeEl.style.marginTop='0px';
+      //timeEl.style.marginTop=window.innerHeight/2-timeEl.getPosition().y
+      //                    -parseInt(timeEl.getStyle('height'))/2
+      //                    +'px';
+      document.documentElement['classList'].add('big');
+      setSize();
+    }
+    //}}}
+    function unbigger()
+    //{{{
+    {
+      //timeEl.style.marginTop='0px';
+      document.documentElement['classList'].remove('big');
+      setSize();
+    }
+    //}}}
+    ml.fullscreenElement(bigger,unbigger,[$('time')],'f');
+  })();
+  //}}}
+//}}}
+})();
+
+/* TIME */
 var domBeat;
 var spark;
-(function()
+(function(){
 //{{{
-{
   var content   =document.getElementById('content');
 
   var lastMinutes,
@@ -123,224 +358,30 @@ var spark;
       window.setTimeout(arguments.callee,300);
     })();
   };
-})();
 //}}}
-
-var getOpt;
-//{{{
-(function(){
- //generate options
-  getOpt=function(id)
-  //{{{
-  {
-    if(id!=='theme')
-    {
-      var theme = getOpt('theme');
-      var ret   = theme && themes[theme] && themes[theme][id];
-      if(ret) return ret;
-    }
-    var el = $(id);
-    return el.type==='text'||el.nodeName==='SELECT'?el.value:!!el.checked;
-  };
-  //}}}
-
- var opts = [
-   {id:'color_icon'        ,description:'icon color'      ,default_:'#c00'                            },
-   {id:'show_seconds_title',description:'seconds in title',default_:false                             },
-   {id:'show_seconds'      ,description:'seconds'         ,default_:false                             },
-   {id:'12_hour'           ,description:'12-hour'         ,default_:DEFAULT_12HOUR                    },
-   {id:'show_pm'           ,description:'am/pm'           ,default_:true ,dependency:'12_hour'        },
-   {id:'show_date'         ,description:'date'            ,default_:true                              },
-   {id:'show_week'         ,description:'week'            ,default_:false,dependency:'show_date'      },
-   {id:'font_size'         ,description:'font size'       ,default_:MIN_WIDTH.toString()              },
-   {id:'theme'             ,description:'theme'           ,default_:'lobster'                         },
-   {id:'font'              ,description:'font'            ,default_:DEFAULT_FONT,negDependency:'theme'},
-   {id:'color_font'        ,description:'font color'      ,default_:DEFAULT_FCOL,negDependency:'theme'},
-   {id:'bg'                ,description:'background image',default_:DEFAULT_BG  ,negDependency:'theme'}];
-
- var themes = {
-   'simple':{
-      'bg':'#fff',
-      'font':'Syncopate',
-      'color_font':'#333'},
-   'steel':{
-      'bg':'http://good-wallpapers.com/pictures/6357/Gray%20Comb%20Texture.jpg',
-      'font':'Syncopate',
-      'color_font':'#a00'},
-   'lobster':{
-      'bg':'#300',
-      'font':'Lobster',
-      'color_font':'#333'},
-   'digital':{
-      'bg':'black',
-      'font':'Orbitron',
-      'color_font':'#0f0'},
-   'paper':{
-      'bg':'http://wallpaper.goodfon.ru/image/209099-1920x1200.jpg',
-      'font':'Redressed',
-      'color_font':'#111'},
-   'ocean':{
-      'bg':'http://www.hotelclubposeidon.it/grafica/background.jpg',
-      'font':'Michroma',
-      'color_font':'#aaa'},
-   'classy':{
-      'bg':'http://www.fantasy-and-art.com/wp-content/gallery/abstract-wallpapers/between_darkness_and_wonder_black_purity_hd_wallpaper.jpg',
-      'font':'Nothing You Could Do',
-      'color_font':'#00a'}};
-
-  //generate html options
-  //{{{
-  (function()
-  {
-    var optionsEl = document.getElementById('options');
-    for(var i=0;i<opts.length;i++)
-    {
-      var opt = opts[i];
-      opt.dom = document.createElement('span');
-      opt.dom.innerHTML=opt.description;
-      opt.input = document.createElement(opt.id==='font'||opt.id==='theme'?'select':'input');
-        opt.input.id   = opt.id;
-        var isCheckbox = opt.default_===false || opt.default_===true;
-        if(opt.input.nodeName==='INPUT')
-        {
-          opt.input.type = isCheckbox?'checkbox':'text';
-          if(!isCheckbox) opt.input.style.width='35px';
-        }
-        else opt.input.style.width=opt.id==='font'?'90px':'68px';
-        if(isCheckbox) opt.dom.prependChild(opt.input);
-        else           opt.dom. appendChild(opt.input);
-      optionsEl.appendChild(opt.dom);
-    }
-
-    $('description')  .style.opacity='0';
-    $('optionsToggle').style.opacity='0';
-    $('optionsToggle').onclick=function(){document.documentElement['classList']['add']('options');$('optionsToggle').style.opacity='1'};
-    document.body.onclick=function(ev) { var target=ml.getEventSource(ev); if(!ml.isChildOf(target,$('optionsToggle'))&&!ml.isChildOf(target,$('options'))){document.documentElement['classList']['remove']('options');$('optionsToggle').style.opacity=0}};
-
-    $('theme').innerHTML='<option label="custom"></option>';
-    for(var i in themes)
-    {
-      var fop=document.createElement('option');
-      fop.innerHTML=i;
-      $('theme').appendChild(fop);
-    }
-  })();
-  //}}}
-
-  var setBodyFont;
-  //{{{
-  (function()
-  {
-    var fontChanger;
-    function setFont(){if(fontChanger) fontChanger.apply(null,arguments)};
-    setBodyFont=function(){setFont(document.body,getOpt('font'),setSize)};
-    ml.loadScript('http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js',function()
-    //{{{
-    {
-      fontChanger=function(el,fontName,callback)
-      {
-        window['WebFont']['load']({'google':{'families':[fontName]},'active':function()
-        {
-          setTimeout(function()
-          {
-            el.style.fontFamily=fontName;
-            if(callback)callback();
-          },100);
-        }});
-      };
-      setFont($('options'),'Arvo');
-      setBodyFont();
-    });
-    //}}}
-    //load list
-    window['onfontsload']=function(resp)
-    //{{{
-    {
-      var fonts=resp['items'];
-      var val = $('font').value;
-      $('font').innerHTML='';
-      for(var i in fonts)
-      {
-        var fop=document.createElement('option');
-        fop.innerHTML=fonts[i]['family'];
-        $('font').appendChild(fop);
-      }
-      $('font').value=val;
-    };
-    //}}}
-    ml.loadScript('https://www.googleapis.com/webfonts/v1/webfonts?callback=onfontsload&sort=popularity&key=AIzaSyAOMrdvfJJPa1btlQNCkXT9gcA-lCADPeE');
-  })();
-  //}}}
-
-  //refresh options onchange
-  //{{{
-  (function()
-  {
-    function setOptVisibility()
-    {
-      for(var i=0;i<opts.length;i++)
-      {
-        var opt = opts[i];
-        var toHide=opt.dependency && !getOpt(opt.dependency) || opt.negDependency && getOpt(opt.negDependency);
-          opt.dom.style.width     =toHide?'0px'   :'auto';
-          opt.dom.style.height    =toHide?'0px'   :'auto';
-          opt.dom.style.visibility=toHide?'hidden':'visible';
-          opt.dom.style.position  =toHide?'absolute':'';
-          opt.dom.style.zIndex    =toHide?'-1':'';
-      }
-    }
-    function colorChangeListener(){document.documentElement.style.color=getOpt('color_font')}
-    function themeChangeListener(){colorChangeListener();setBodyFont();bgChanger(getOpt('bg'));setOptVisibility()}
-
-    var bgChanger;
-    for(var i=0;i<opts.length;i++)
-    {
-      var opt = opts[i];
-      if(opt.id==='bg') bgChanger=ml.htmlBackground(opt.input.id,opt.dom,opt.default_);
-      else
-      {
-        var changeListener;
-        if(opt.id==='color_font') changeListener=colorChangeListener;
-        else if(opt.id==='theme') changeListener=themeChangeListener;
-        else if(opt.id==='font')  changeListener=setBodyFont;
-        else                      changeListener=function(){domBeat(true);setOptVisibility()};
-        ml.persistantInput(opt.id,changeListener,opt.default_,0,true);
-      }
-    }
-    themeChangeListener();
-  })();
-  //}}}
-
-  //fullscreen
-  //{{{
-  (function()
-  {
-    function bigger()
-    //{{{
-    {
-      //document.body.webkitRequestFullScreen();
-      //timeEl.style.marginTop='0px';
-      //timeEl.style.marginTop=window.innerHeight/2-timeEl.getPosition().y
-      //                    -parseInt(timeEl.getStyle('height'))/2
-      //                    +'px';
-      document.documentElement['classList'].add('big');
-      setSize();
-    }
-    //}}}
-    function unbigger()
-    //{{{
-    {
-      //timeEl.style.marginTop='0px';
-      document.documentElement['classList'].remove('big');
-      setSize();
-    }
-    //}}}
-    ml.fullscreenElement(bigger,unbigger,[$('time')],'f');
-  })();
-  //}}}
 })();
-//}}}
 
 spark();
+})();
 
+//load font list
+(function(){
+//{{{
+  window['onfontsload']=function(resp)
+  {
+    var fonts=resp['items'];
+    var val = $('font').value;
+    $('font').innerHTML='';
+    var max=300;//firefox crashes when selecting a from too many options
+    for(var i in fonts)
+    {
+      if(ml.browser().usesGecko && !max--)break;
+      var fop=document.createElement('option');
+      fop.innerHTML=fonts[i]['family'];
+      $('font').appendChild(fop);
+    }
+    $('font').value=val;
+  };
+  ml.loadScript('https://www.googleapis.com/webfonts/v1/webfonts?callback=onfontsload&sort=popularity&key=AIzaSyAOMrdvfJJPa1btlQNCkXT9gcA-lCADPeE');
+//}}}
 })();
