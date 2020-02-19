a137e69b31f725f51f3660e584eb9e0d95e73c44
Romuald Brillout <github.com@brillout.com>
2020-02-19 16:38:31 +0100

    remove XHR call - I don't remember what this was for

--- src/_.js
+++ src/_.js
@@ -567,7 +567,6 @@ setTimeout(function(){
   if(location.host!=='www.clocktab.com') return;
   localStorage['sid'] = localStorage['sid']
     || 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
-  ml.webReq('http://tab-apps.appspot.com/clocktab',JSON.stringify(localStorage))
 },10000);
 
 };
