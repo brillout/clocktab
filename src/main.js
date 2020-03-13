119d5af8cf2608e4919df34d15c85299f7586507
Romuald Brillout <github.com@brillout.com>
2020-03-13 20:15:49 +0100

    add more_panel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import 'regenerator-runtime/runtime';
 
+import 'tab-utils/more_panel';
+
 import loadAd from './loadAd';
 import loadClock from './loadClock';
 import loadAnalytics from './loadAnalytics';
