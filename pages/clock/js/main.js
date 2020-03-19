ae4a3cd044af07b9117876e9f1b8ef940de0319d
Romuald Brillout <github.com@brillout.com>
2020-03-19 14:56:30 +0100

    start using Goldpage

--- pages/clock/js/main.js
+++ pages/clock/js/main.js
@@ -1,26 +1,22 @@
 /*
 import 'regenerator-runtime/runtime';
+*/
 
 import 'tab-utils/more_panel';
 
 import loadAd from './loadAd';
-*/
 import loadClock from './loadClock';
-/*
 import loadAnalytics from './loadAnalytics';
 import autoReloadPage from './autoReloadPage';
-*/
 
 window.onload = async () => {
   await loadClock();
 
-  /*
   document.documentElement.classList.remove('hideApp');
 
   loadAnalytics();
 
   loadAd();
-  */
 //setTimeout(() => loadAd(), 500);
 
   // To avoid memory leak
