9fa400d5d0d12c50208961b6d350bb1e5dbb89fa
Romuald Brillout <github.com@brillout.com>
2020-03-19 13:36:45 +0100

    start using Goldpage

--- pages/clock/ClockView.js
+++ pages/clock/ClockView.js
@@ -3,5 +3,13 @@ import React from 'react';
 export default ClockView;
 
 function ClockView() {
-  return <div>hhh</div>
+  return (
+    <>
+      <div>1</div>
+      <div>2</div>
+    </>
+  );
 }
+/*
+<div dangerouslySetInnerHTML={{__html: "<script>console.log('hello')</script>"}} />;
+*/
