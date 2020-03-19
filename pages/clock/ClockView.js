b57e2cf300b50d327cd9a7695101a30640381c17
Romuald Brillout <github.com@brillout.com>
2020-03-19 12:22:21 +0100

    start using Goldpage

---
+++ pages/clock/ClockView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ClockView;
+
+function ClockView() {
+  return <div>hhh</div>
+}
---
+++ pages/clock/clock.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClockView from './ClockView';
+
+export default {
+  route: '/',
+  view: ClockView,
+
+  title: 'Clock Tab',
+
+  renderToDom: false,
+  renderToHtml: true,
+};
+
