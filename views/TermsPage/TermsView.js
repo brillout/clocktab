227616d872f185acf218baca74fc68812e1a645e
Romuald Brillout <github.com@brillout.com>
2020-03-21 22:37:55 +0100

    add pages

---
+++ views/TermsPage/TermsView.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+export default TermsView;
+
+const projectName = 'Clock Tab';
+const projectWebsite = 'https://www.clocktab.com';
+let projectEmail = 'clocktab';
+projectEmail+='@';
+projectEmail+='brillout.com';
+
+function TermsView() {
+  return <>
+
+<h1>Terms of Service</h1>
+
+<h2>1. Acceptance of Terms</h2>
+
+<p>
+{projectName} provides the information on this Web site as provided in these Terms of Service ("TOS"). {projectName} may update the TOS at any time and without prior notice by posting a new version at {projectWebsite}. The information on this site and your use of it is subject to the most recent version of the TOS posted.
+</p>
+
+<h2>2. Description of Service</h2>
+
+<p>
+{projectName} provides you with access to discussion lists, forums, licenses and a variety of other services. ("{projectName} Service"). Unless explicitly stated otherwise, any new features that augment or enhance the current {projectName} Service shall be subject to the TOS.
+</p>
+
+<h2>3. Your Registration Obligations</h2>
+
+<p>
+In consideration of your use of the {projectName} Service, you represent that you are of legal age to form a binding contract and are not a person barred from receiving services under the laws of the United States or other applicable jurisdiction. You also agree to:
+</p>
+
+<p>
+1. provide true, accurate, current and complete information about yourself as prompted by the {projectName} Service's registration form (the "Registration Data") and
+2. maintain and promptly update the Registration Data to keep it true, accurate, current and complete. If you provide any information that is untrue, inaccurate, not current or incomplete, or {projectName} has reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, {projectName} has the right to suspend or terminate your account and refuse any and all current or future use of the {projectName} Services (or any portion thereof).
+</p>
+
+<h2>4. {projectName} Privacy Policy</h2>
+
+<p>
+Registration Data and certain other information about you is subject to our Privacy Policy.
+</p>
+
+<h2>5. Conduct</h2>
+
+<p>
+You understand that all information, data, text, software, graphics or other materials ("Content"), whether publicly posted or privately transmitted, are the sole responsibility of the person from whom such Content originated. This means that you, and not {projectName}, are entirely responsible for all Content that you upload, post, email, transmit or otherwise make available via the {projectName} Service. {projectName} does not control the Content posted via the {projectName} Service and, as such, does not guarantee the accuracy, integrity or quality of such Content. Under no circumstances will {projectName} be liable in any way for any Content, including, but not limited to, any errors or omissions in any Content, or any loss or damage of any kind incurred as a result of the use of any Content posted, emailed, transmitted or otherwise made available via the {projectName} Service. You agree to not use the {projectName} Service to:
+</p>
+
+<p>
+1. upload, post, email, transmit or otherwise make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortuous, defamatory, vulgar, obscene, libelous, invasive of another's privacy, hateful, or racially, ethnically or otherwise objectionable or harm minors;
+2. impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person or entity;
+3. forge headers or otherwise manipulate identifiers in order to disguise the origin of any Content transmitted through the {projectName} Service;
+4. upload, post, email, transmit or otherwise make available any Content that you do not have a right to make available under any law or under contractual or other relationships;
+5. upload, post, email, transmit or otherwise make available any Content that infringes any patent, trademark, trade secret, copyright or other proprietary rights of any party;
+6. upload, post, email, transmit or otherwise make available any unsolicited or unauthorized advertising, promotional materials, "junk mail," "spam," or any other form of solicitation;
+7. upload, post, email, transmit or otherwise make available any material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment;
+8. disrupt the normal flow of dialogue, or otherwise act in a manner that negatively affects other users' ability to engage in discussions or exchanges;
+9. interfere with or disrupt the {projectName} Service or servers or networks connected to the {projectName} Service, or disobey any requirements, procedures, policies or regulations of networks connected to the {projectName} Service;
+10. intentionally or unintentionally violate any applicable local, state, national or international law,
+11. "stalk" or otherwise harass another; and/or
+12. collect or store personal data about other users in connection with the prohibited conduct and activities set forth in paragraphs above. You acknowledge that {projectName} may or may not pre-screen Content, but that {projectName} and its designees shall have the right (but not the obligation) in their sole discretion to pre-screen, refuse, move, or remove any Content that is available via the {projectName} Service and which violates the TOS. You agree that you must evaluate, and bear all risks associated with, the use of any Content, including any reliance on the accuracy, completeness, or usefulness of such Content. You acknowledge, consent and agree that {projectName} may access, preserve and disclose your account information and Content if required to do so by law or in a good faith belief that such access preservation or disclosure is reasonably necessary to:
+    1. comply with legal process;
+    2. enforce the TOS;
+    3. respond to claims that any Content violates the rights of third parties;
+    4. respond to your requests for customer service; or (e) protect the rights, property or personal safety of {projectName} its users and the public.
+</p>
+
+<h2>6. Content Made Available For Inclusion on the {projectName} Service</h2>
+
+<p>
+{projectName} does not claim ownership of Content you submit or make available for inclusion via the {projectName} Service. However, with respect to Content you submit or make available for inclusion on the publicly available {projectName} Service, you irrevocably grant {projectName} the perpetual, worldwide, royalty-free and non-exclusive license, with the right to sublicense through multiple tiers of sublicensees, to use, distribute, reproduce, modify, adapt, publicly perform and publicly display such Content, in whole or in part, on the {projectName} Service or other publications by {projectName} in any media whether now existing or which come into the existence into the future and to provide that information under the license set forth on the initial page of the {projectWebsite} Web site. And for Content you submit for private discussions, you grant {projectName} the worldwide, royalty-free and non-exclusive license to use, distribute, reproduce, modify, adapt, publicly perform and publicly display such Content on the associated private discussion group.
+</p>
+
+<h2>7. Indemnity</h2>
+
+<p>
+You agree to indemnify and hold {projectName} and its subsidiaries, affiliates, officers, agents, employees, partners and licensors harmless from any claim or demand, including but not limited to reasonable attorneys' fees, made by any third party due to or arising out of Content you submit, post, transmit or otherwise make available through the Service, your use of the {projectName} Service, your connection to the Service, your violation of the TOS, or your violation of any rights of another.
+</p>
+
+<h2>8. Modifications to {projectName} Service</h2>
+
+<p>
+You acknowledge that {projectName} may establish general practices and limits concerning use of the {projectName} Service, including without limitation the maximum number of days that email messages, message board postings or other uploaded Content will be retained by the {projectName} Service.. You further acknowledge that {projectName} reserves the right to modify these general practices and limits from time to time. {projectName} reserves the right at any time and from time to time to modify or discontinue, temporarily or permanently, the {projectName} Service (or any part thereof) with or without notice. You agree that {projectName} shall not be liable to you or to any third party for any modification, suspension or discontinuance of the {projectName} Service.
+</p>
+
+<h2>9. Termination</h2>
+
+<p>
+You agree that {projectName} may terminate your access to the {projectName} Service for violations of the TOS and/or requests by authorized law enforcement or other government agencies.
+</p>
+
+<h2>10. Links</h2>
+
+<p>
+The {projectName} Service may provide, or third parties may provide, links to other World Wide Web sites or resources. Because {projectName} has no control over such sites and resources, you acknowledge and agree that {projectName} is not responsible for the availability of such external sites or resources, and does not endorse and is not responsible or liable for any Content, advertising, products or other materials on or available from such sites or resources. You further acknowledge and agree that {projectName} shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such Content, goods or services available on or through any such site or resource.
+</p>
+
+<h2>11. Disclaimer of Warranties</h2>
+
+<p>
+You expressly understand and agree that: Your use of the {projectName} service is at your sole risk. {projectName} service is provided on an "as is" and "as available" basis. {projectName} and its subsidiaries, affiliates, officers, employees and licensors expressly disclaim all warranties of any kind, whether express or implied, including, but not limited to the implied warranties of merchantability, fitness for a particular purpose and non-infringement. {projectName} and its subsidiaries, affiliates, officers, employees and licensors make no warranty that
+</p>
+
+<p>
+1. The {projectName} service will meet your requirements;
+2. The {projectName} service will be uninterrupted, timely, secure or error-free and
+3. The results that may be obtained from the use of the {projectName} service will be accurate or reliable. No advice or information, whether oral or written, obtained by you from {projectName} or through or from the service shall create any warranty not expressly stated in the TOS.
+</p>
+
+<h2>12. Limitation of Liability</h2>
+
+<p>
+You expressly understand and agree that {projectName} and its subsidiaries, affiliates, officers, employees, agents, partners and licensors shall not be liable to you for any direct, indirect, incidental, special, consequential or exemplary damages, including, but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if {projectName} has been advised of the possibility of such damages), resulting from the use or the inability to use the {projectName} service.
+</p>
+
+<h2>13. Notice and Procedure for Making Claims of Copyright Infringement</h2>
+
+<p>
+{projectName} respects the copyright of others, and we ask our users to do the same. {projectName} may, in appropriate circumstances and at its discretion, disable and/or terminate the access of users who may be repeat infringers. If you believe that your work has been copied in a way that constitutes copyright infringement, please provide {projectName} the following information: an electronic or physical signature of the person authorized to act on behalf of the owner of the copyright; a description of the copyrighted work that you claim has been infringed; a description of where the material that you claim is infringing is located on the site; your address, telephone number, and email address; a statement by you that you have a good faith belief that the disputed use is not authorized by the copyright owner, its agent, or the law; a statement by you, made under penalty of perjury, that the above information in your Notice is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf.
+</p>
+
+<p>
+{projectName} can be reached for notice of claims of copyright infringement at: {projectEmail}.
+</p>
+
+<h2>14. General Information</h2>
+
+<p>
+Entire Agreement. The TOS constitutes the entire agreement between you and {projectName} and governs your use of the {projectName} Service, superseding any prior agreements between you and {projectName} with respect to the {projectName} Service. Choice of Law and Forum. The TOS and the relationship between you and {projectName} shall be governed by the laws of Germany without regard to its conflict of law provisions. You and {projectName} agree to submit to the personal and exclusive jurisdiction of the courts located within Germany. Waiver and Severability of Terms. The failure of {projectName} to exercise or enforce any right or provision of the TOS shall not constitute a waiver of such right or provision. If any provision of the TOS is found by a court of competent jurisdiction to be invalid, the parties nevertheless agree that the court should endeavor to give effect to the parties' intentions as reflected in the provision, and the other provisions of the TOS remain in full force and effect.
+</p>
+
+  </>;
+}
---
+++ views/TermsPage/index.page.js
@@ -0,0 +1,5 @@
+import {getPageConfig} from '../PageWrapper';
+import TermsView from './TermsView';
+
+export default getPageConfig(TermsView, 'Terms of Service', {route: '/terms'});
+
