dbdd53c06526012a29869cad0b916cca562b3123
Romuald Brillout <github.com@brillout.com>
2020-03-21 22:43:32 +0100

    add pages

--- views/TermsPage/TermsView.js
+++ views/TermsPage/TermsView.js
@@ -29,11 +29,10 @@ function TermsView() {
 
 <p>
 In consideration of your use of the {projectName} Service, you represent that you are of legal age to form a binding contract and are not a person barred from receiving services under the laws of the United States or other applicable jurisdiction. You also agree to:
-</p>
-
-<p>
-1. provide true, accurate, current and complete information about yourself as prompted by the {projectName} Service's registration form (the "Registration Data") and
-2. maintain and promptly update the Registration Data to keep it true, accurate, current and complete. If you provide any information that is untrue, inaccurate, not current or incomplete, or {projectName} has reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, {projectName} has the right to suspend or terminate your account and refuse any and all current or future use of the {projectName} Services (or any portion thereof).
+  <ol>
+    <li>provide true, accurate, current and complete information about yourself as prompted by the {projectName} Service's registration form (the "Registration Data") and</li>
+    <li>maintain and promptly update the Registration Data to keep it true, accurate, current and complete. If you provide any information that is untrue, inaccurate, not current or incomplete, or {projectName} has reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, {projectName} has the right to suspend or terminate your account and refuse any and all current or future use of the {projectName} Services (or any portion thereof).</li>
+  </ol>
 </p>
 
 <h2>4. {projectName} Privacy Policy</h2>
@@ -46,25 +45,26 @@ Registration Data and certain other information about you is subject to our Priv
 
 <p>
 You understand that all information, data, text, software, graphics or other materials ("Content"), whether publicly posted or privately transmitted, are the sole responsibility of the person from whom such Content originated. This means that you, and not {projectName}, are entirely responsible for all Content that you upload, post, email, transmit or otherwise make available via the {projectName} Service. {projectName} does not control the Content posted via the {projectName} Service and, as such, does not guarantee the accuracy, integrity or quality of such Content. Under no circumstances will {projectName} be liable in any way for any Content, including, but not limited to, any errors or omissions in any Content, or any loss or damage of any kind incurred as a result of the use of any Content posted, emailed, transmitted or otherwise made available via the {projectName} Service. You agree to not use the {projectName} Service to:
-</p>
-
-<p>
-1. upload, post, email, transmit or otherwise make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortuous, defamatory, vulgar, obscene, libelous, invasive of another's privacy, hateful, or racially, ethnically or otherwise objectionable or harm minors;
-2. impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person or entity;
-3. forge headers or otherwise manipulate identifiers in order to disguise the origin of any Content transmitted through the {projectName} Service;
-4. upload, post, email, transmit or otherwise make available any Content that you do not have a right to make available under any law or under contractual or other relationships;
-5. upload, post, email, transmit or otherwise make available any Content that infringes any patent, trademark, trade secret, copyright or other proprietary rights of any party;
-6. upload, post, email, transmit or otherwise make available any unsolicited or unauthorized advertising, promotional materials, "junk mail," "spam," or any other form of solicitation;
-7. upload, post, email, transmit or otherwise make available any material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment;
-8. disrupt the normal flow of dialogue, or otherwise act in a manner that negatively affects other users' ability to engage in discussions or exchanges;
-9. interfere with or disrupt the {projectName} Service or servers or networks connected to the {projectName} Service, or disobey any requirements, procedures, policies or regulations of networks connected to the {projectName} Service;
-10. intentionally or unintentionally violate any applicable local, state, national or international law,
-11. "stalk" or otherwise harass another; and/or
-12. collect or store personal data about other users in connection with the prohibited conduct and activities set forth in paragraphs above. You acknowledge that {projectName} may or may not pre-screen Content, but that {projectName} and its designees shall have the right (but not the obligation) in their sole discretion to pre-screen, refuse, move, or remove any Content that is available via the {projectName} Service and which violates the TOS. You agree that you must evaluate, and bear all risks associated with, the use of any Content, including any reliance on the accuracy, completeness, or usefulness of such Content. You acknowledge, consent and agree that {projectName} may access, preserve and disclose your account information and Content if required to do so by law or in a good faith belief that such access preservation or disclosure is reasonably necessary to:
-    1. comply with legal process;
-    2. enforce the TOS;
-    3. respond to claims that any Content violates the rights of third parties;
-    4. respond to your requests for customer service; or (e) protect the rights, property or personal safety of {projectName} its users and the public.
+<ol>
+<li>upload, post, email, transmit or otherwise make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortuous, defamatory, vulgar, obscene, libelous, invasive of another's privacy, hateful, or racially, ethnically or otherwise objectionable or harm minors;</li>
+<li>impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person or entity;</li>
+<li>forge headers or otherwise manipulate identifiers in order to disguise the origin of any Content transmitted through the {projectName} Service;</li>
+<li>upload, post, email, transmit or otherwise make available any Content that you do not have a right to make available under any law or under contractual or other relationships;</li>
+<li>upload, post, email, transmit or otherwise make available any Content that infringes any patent, trademark, trade secret, copyright or other proprietary rights of any party;</li>
+<li>upload, post, email, transmit or otherwise make available any unsolicited or unauthorized advertising, promotional materials, "junk mail," "spam," or any other form of solicitation;</li>
+<li>upload, post, email, transmit or otherwise make available any material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment;</li>
+<li>disrupt the normal flow of dialogue, or otherwise act in a manner that negatively affects other users' ability to engage in discussions or exchanges;</li>
+<li>interfere with or disrupt the {projectName} Service or servers or networks connected to the {projectName} Service, or disobey any requirements, procedures, policies or regulations of networks connected to the {projectName} Service;</li>
+<li> intentionally or unintentionally violate any applicable local, state, national or international law,</li>
+<li> "stalk" or otherwise harass another; and/or</li>
+<li> collect or store personal data about other users in connection with the prohibited conduct and activities set forth in paragraphs above. You acknowledge that {projectName} may or may not pre-screen Content, but that {projectName} and its designees shall have the right (but not the obligation) in their sole discretion to pre-screen, refuse, move, or remove any Content that is available via the {projectName} Service and which violates the TOS. You agree that you must evaluate, and bear all risks associated with, the use of any Content, including any reliance on the accuracy, completeness, or usefulness of such Content. You acknowledge, consent and agree that {projectName} may access, preserve and disclose your account information and Content if required to do so by law or in a good faith belief that such access preservation or disclosure is reasonably necessary to:</li>
+  <ol>
+    <li>comply with legal process;</li>
+    <li>enforce the TOS;</li>
+    <li>respond to claims that any Content violates the rights of third parties;</li>
+    <li>respond to your requests for customer service; or (e) protect the rights, property or personal safety of {projectName} its users and the public.</li>
+  </ol>
+</ol>
 </p>
 
 <h2>6. Content Made Available For Inclusion on the {projectName} Service</h2>
@@ -101,12 +101,11 @@ The {projectName} Service may provide, or third parties may provide, links to ot
 
 <p>
 You expressly understand and agree that: Your use of the {projectName} service is at your sole risk. {projectName} service is provided on an "as is" and "as available" basis. {projectName} and its subsidiaries, affiliates, officers, employees and licensors expressly disclaim all warranties of any kind, whether express or implied, including, but not limited to the implied warranties of merchantability, fitness for a particular purpose and non-infringement. {projectName} and its subsidiaries, affiliates, officers, employees and licensors make no warranty that
-</p>
-
-<p>
-1. The {projectName} service will meet your requirements;
-2. The {projectName} service will be uninterrupted, timely, secure or error-free and
-3. The results that may be obtained from the use of the {projectName} service will be accurate or reliable. No advice or information, whether oral or written, obtained by you from {projectName} or through or from the service shall create any warranty not expressly stated in the TOS.
+<ol>
+  <li>The {projectName} service will meet your requirements;</li>
+  <li>The {projectName} service will be uninterrupted, timely, secure or error-free and</li>
+  <li>The results that may be obtained from the use of the {projectName} service will be accurate or reliable. No advice or information, whether oral or written, obtained by you from {projectName} or through or from the service shall create any warranty not expressly stated in the TOS.</li>
+</ol>
 </p>
 
 <h2>12. Limitation of Liability</h2>
