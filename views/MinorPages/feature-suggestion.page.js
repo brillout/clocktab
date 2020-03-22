0bee7cbc943a7ed15ce78aeb372b42a730bc1111
Romuald Brillout <github.com@brillout.com>
2020-03-22 13:26:10 +0100

    add pages

---
+++ views/MinorPages/feature-suggestion.page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {getPageConfig} from '../PageWrapper';
+
+export default getPageConfig(
+  () => <>
+    To suggest a feature, write an email at
+    <a
+      data-subject="Feature Suggestion"
+      data-body="Hi Romuald, I'd like to suggest following new feature:\n\n"
+      className="contact-address"
+      target="_blank"
+    />.
+  </>,
+  'Feature Suggestion',
+);
