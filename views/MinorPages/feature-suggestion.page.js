693b7072840c54820b2d4c9132ab655ea633091c
Romuald Brillout <github.com@brillout.com>
2020-03-22 13:34:47 +0100

    minor

--- views/MinorPages/feature-suggestion.page.js
+++ views/MinorPages/feature-suggestion.page.js
@@ -3,14 +3,25 @@ import {getPageConfig} from '../PageWrapper';
 
 export default getPageConfig(
   () => <>
-    To suggest a feature, write an email at{' '}
+    To suggest a feature, write an email to
+    {' '}
     <a
       data-subject="Feature Suggestion"
-      data-body="Hi Romuald, I'd like to suggest following new feature:\n\n"
+      data-body="Hi Romuald, I'd like to suggest following new feature:"
       className="contact-address"
       target="_blank"
     />
     .
+
+    <p>
+    If you have a GitHub account, you can also
+    {' '}
+    <a href="https://github.com/brillout/clocktab/issues/new" target="_blank">
+      create a GitHub ticket
+    </a>
+    {' '}
+    on Clock Tab's repository.
+    </p>
   </>,
   'Feature Suggestion',
 );
