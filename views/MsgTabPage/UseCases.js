5655ec18d63780018bb0aab1ea7074408693d14d
Romuald Brillout <github.com@brillout.com>
2020-03-25 10:07:26 +0100

    minor

---
+++ views/MsgTabPage/UseCases.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default UseCases;
+
+function UseCases() {
+  return (
+    <Center>
+      <Title>Msg Tab Use Cases</Title>
+      <Item>School & Universities - Communicate to students without speaking, e.g. during examination.</Item>
+      <Item>At the library - You are forbidden to talk.</Item>
+      <Item>Audio not working - At a video coference or at presentation with a large audience.</Item>
+      <Item>For Fun.</Item>
+    </Center>
+  );
+}
+function Center({children}) {
+  return (
+    <div style={{textAlign: 'center', padding: '30px 0'}}>
+      <div style={{textAlign: 'left', display: 'inline-block'}}>
+        {children}
+      </div>
+    </div>
+  );
+}
+function Title({children}) {
+  return (
+    <span style={{
+      textDecoration: 'underline',
+      fontSize: '1.2em',
+      /*
+      display: 'block',
+      textAlign: 'center',
+      */
+    }}>
+      {children}
+    </span>
+  );
+}
+function Item({children}) {
+  return <div style={{paddingLeft: 7, marginTop: 5}}>• {children}</div>;
+}
--- views/MsgTabPage/msg-tab.page.js
+++ views/MsgTabPage/msg-tab.page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {FullView, MorePanel, config} from '../FullViewWrapper';
 import './ml.js';
 import './msg-tab.css';
+import UseCases from './UseCases';
 import init_msg_tab from './init_msg_tab';
 
 export default config({
@@ -25,43 +26,3 @@ export default config({
     init_msg_tab({text});
   },
 });
-
-
-function UseCases() {
-  return (
-    <Center>
-      <Title>Msg Tab Use Cases</Title>
-      <Item>School - Communicate to students without speaking, e.g. during examination.</Item>
-      <Item>Presentation - Audio is not working.</Item>
-      <Item>Video Conference - Audio is not working.</Item>
-      <Item>At the library - You are forbidden to talk.</Item>
-      <Item>For Fun.</Item>
-    </Center>
-  );
-}
-function Center({children}) {
-  return (
-    <div style={{textAlign: 'center', padding: '30px 0'}}>
-      <div style={{textAlign: 'left', display: 'inline-block'}}>
-        {children}
-      </div>
-    </div>
-  );
-}
-function Title({children}) {
-  return (
-    <span style={{
-      textDecoration: 'underline',
-      fontSize: '1.2em',
-      /*
-      display: 'block',
-      textAlign: 'center',
-      */
-    }}>
-      {children}
-    </span>
-  );
-}
-function Item({children}) {
-  return <div style={{paddingLeft: 7, marginTop: 5}}>• {children}</div>;
-}
