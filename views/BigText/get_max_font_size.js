5e264182f355377a716c02d0cf9cb442f431393c
Romuald Brillout <github.com@brillout.com>
2020-03-27 13:33:37 +0100

    improve font-size calculation

--- views/BigText/get_max_font_size.js
+++ views/BigText/get_max_font_size.js
@@ -6,51 +6,25 @@ function get_max_font_size({dom_el, max_width, max_height}) {
   const el = dom_el;
   const w = max_width;
   const h = max_height;
-  return getTextSize(el, w, h);
+
+  const {fontSize, height, width} = getEstimation(el,w,h)
+  assert(fontSize);
+  assert(height);
+  assert(width);
+  return {fontSize, height, width};
 }
 
 // export {adjustFontSize};
 
-function getDummy(tagName) { 
-  var dummy = document.createElement(tagName||'div');
-  dummy.style.display='inline-block';
-  dummy.style.position='absolute';
-  dummy.style.top='0';
-  dummy.style.top='-9999px';
-  dummy.style.zIndex='-9999';
-  dummy.style.visibility='hidden';
-  return dummy;
-} 
+function getEstimation(el,width,height,possibleChars,minTextLength){ 
+  const DUMMY_SIZE=100;//intuitively: the bigger the font-size the more precise the approximation
 
-function getWidestChar(chars) { 
-  var widestChar;
-  var widestSize=-1;
-  var dummy = document.body.appendChild(getDummy());
-  for(var i=0;i<chars.length;i++) {
-    dummy.innerHTML=chars[i];
-    var charWidth=parseInt(get_el_style(dummy,'width'),10);
-    if(charWidth>widestSize)
-    {
-      widestSize=charWidth;
-      widestChar=chars[i];
+  var dummyContent=el.innerHTML;
+    if( minTextLength ){
+      if( dummyContent.length<minTextLength&&minTextLength.length ) {
+        dummyContent=minTextLength;
+      }
     }
-  }
-
-  //window.bla=window.bla||0;
-  //if(window.bla!==2&&window.bla!==0) document.body.removeChild(dummy);
-  //window.bla++;
-
-  //dummyinspect
-  //onsole
-  // TODO
-  document.body.removeChild(dummy);
-  assert.warning(widestChar);
-  return widestChar;
-} 
-
-function getData(el,width,height,possibleChars,minTextLength){ 
-  var dummyContent=el.getAttribute('data-text')+el.innerHTML;
-    if(dummyContent.length<(minTextLength&&minTextLength.length)) dummyContent=minTextLength;
     if(dummyContent.length<1) dummyContent='y';
     if(possibleChars) //if possibleChars not given we then assume that all char have same width
     {
@@ -63,10 +37,10 @@ function getData(el,width,height,possibleChars,minTextLength){
 
   var dummy= getDummy(el.tagName);
     dummy.style.fontFamily=get_el_style(el,'font-family');
-    dummy.style.fontFamily=get_el_style(el,'font-family');
     dummy.style.fontSize=DUMMY_SIZE+'px';
     dummy.style.whiteSpace='nowrap';//should el be equal to get_el_style('white-space')?
     dummy.style.letterSpacing=get_el_style(el,'letter-spacing');
+    dummy.style.lineHeight=get_el_style(el,'line-height');
 
   dummy.innerHTML=dummyContent;
   document.body.appendChild(dummy);
@@ -109,19 +83,21 @@ function getData(el,width,height,possibleChars,minTextLength){
   var height_dummy = height&&parseInt(get_el_style(dummy,'height'),10);
   var ratio = Math.min(height?(height/height_dummy):Infinity,
                         width?( width/ width_dummy):Infinity);
-  return {fontSize:ratio*DUMMY_SIZE,width:ratio*width_dummy,height:ratio*height_dummy};
+  return {
+    fontSize:ratio*DUMMY_SIZE,
+    width:ratio*width_dummy,
+    height:ratio*height_dummy,
+  };
 } 
 
-const DUMMY_SIZE=100;//intuitively: the bigger the font-size the more precise the approximation
 var boxSizingPropName=null;
-
 function adjustFontSize(el,possibleChars,noHeight,minTextLength) { 
   //using cache -> assumption is made that font familly and box size doesn't change
   //based on assumption that width of a text is approx proportional to its fontSize
   //notes
   //-absolute font-size is equally precise as percentage font-size
   //-possibleChars is used for fonts with variable char width
-  var textLength = (el.getAttribute('data-text')+el.innerHTML).length;
+  var textLength = el.innerHTML.length;
   if(textLength===el._oldTextLength) return;
   el._oldTextLength=textLength;
   if(!el._ml_textSizeRatioCache) el._ml_textSizeRatioCache={};
@@ -146,8 +122,7 @@ function adjustFontSize(el,possibleChars,noHeight,minTextLength) {
     height -= getSize(el,'border-top' )+getSize(el,'border-bottom')+getSize(el,'padding-top' )+getSize(el,'padding-bottom');
   }
 
-  
-  el.style.fontSize=Math.floor(getData(el,width,height,possibleChars,minTextLength).fontSize)+'px';
+  el.style.fontSize=Math.floor(getEstimation(el,width,height,possibleChars,minTextLength).fontSize)+'px';
 
   assert.warning(get_el_style(el,'display')==='block' || get_el_style(el,'display')==='inline-block' || get_el_style(el,'display')==='table-cell',"get_el_style(el,'display')=="+get_el_style(el,'display'),1);
 
@@ -169,8 +144,44 @@ function adjustFontSize(el,possibleChars,noHeight,minTextLength) {
   */
 }; 
 
-function getTextSize(el,w,h){return getData(el,w,h)};
+function getDummy(tagName) { 
+  var dummy = document.createElement(tagName||'div');
+  dummy.style.display='inline-block';
+  dummy.style.position='absolute';
+  dummy.style.top='0';
+  dummy.style.top='-9999px';
+  dummy.style.zIndex='-9999';
+  dummy.style.visibility='hidden';
+  return dummy;
+} 
+
+function getWidestChar(chars) { 
+  var widestChar;
+  var widestSize=-1;
+  var dummy = document.body.appendChild(getDummy());
+  for(var i=0;i<chars.length;i++) {
+    dummy.innerHTML=chars[i];
+    var charWidth=parseInt(get_el_style(dummy,'width'),10);
+    if(charWidth>widestSize)
+    {
+      widestSize=charWidth;
+      widestChar=chars[i];
+    }
+  }
+
+  //window.bla=window.bla||0;
+  //if(window.bla!==2&&window.bla!==0) document.body.removeChild(dummy);
+  //window.bla++;
+
+  //dummyinspect
+  //onsole
+  // TODO
+  document.body.removeChild(dummy);
+  assert.warning(widestChar);
+  return widestChar;
+} 
 
 function get_el_style(el, styleProp) {
   return document.defaultView.getComputedStyle(el,null).getPropertyValue(styleProp);
 }
+
