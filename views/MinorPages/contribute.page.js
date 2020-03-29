65246e9a1486ad19940674bc5fb3537588e887e0
Romuald Brillout <github.com@brillout.com>
2020-03-29 12:13:49 +0200

    minor

--- views/MinorPages/contribute.page.js
+++ views/MinorPages/contribute.page.js
@@ -4,19 +4,13 @@ import {getPageConfig} from '../PageWrapper';
 export default getPageConfig(
   () => <>
     <p>
-    Clock Tab's source code is on GitHub and the best way
-    to contribute is to participate in the development of Clock Tab's source code.
+    The best way to contribute is to <a href="/donate">donate</a>.
     </p>
 
     <p>
-    If you know how to program code,
-    then head over to <a href="https://github.com/brillout/clocktab" target="_blank">Clock Tab's GitHub</a>.
-    </p>
-
-    <p>
-    If don't know how to programm
-    but you want to learn how to code,
-    write an email to Romuald at <a data-subject="I'd like to contribute" className="contact-address"></a>.
+    Or, if you know how to program, you can contribute by participating in Clock Tab's development, see
+    {" "}
+    <a href="https://github.com/brillout/clocktab" target="_blank">Clock Tab's GitHub</a>.
     </p>
   </>,
   'Contribute',
