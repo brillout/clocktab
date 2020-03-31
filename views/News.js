38d508f2c301f74fac23ca7ba6596eddd221e387
Romuald Brillout <github.com@brillout.com>
2020-03-31 14:34:45 +0200

    minor

--- views/News.js
+++ views/News.js
@@ -13,7 +13,7 @@ function News({preset_concept_name, ...props}) {
       <li>{preset_concept_name} super-powered &mdash; you can now create, save, and share {preset_concept_name}s!</li>
       */}
       <li>Themes super-powered &mdash; you can now create, save, and share Themes.</li>
-      <li>World Clock &mdash; to create multiple Clocks and conert timezones.</li>
+      <li>World Clock &mdash; to create multiple Clocks and convert timezones.</li>
       <li>Msg Tab &mdash; Communication-over-Screen, especially useful in education.</li>
     </ul>
     These features are new; if you encounter any issue then <a href="/bug-repair">let me know</a>.
