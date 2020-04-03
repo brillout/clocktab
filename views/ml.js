260cc007f1f99b4d5bc0b94fffb586a46e9559c0
Romuald Brillout <github.com@brillout.com>
2020-04-03 18:44:25 +0200

    make ml.date.getWeek safer

--- views/ml.js
+++ views/ml.js
@@ -114,19 +114,21 @@ ml.date={};
   //Date.prototype.getWeek = function (dowOffset) {
   /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
 
-    var dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
-    var newYear = new Date(that.getFullYear(),0,1);
-    var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+ // var dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
+    const dowOffset = 0;
+
+    let newYear = new Date(that.getFullYear(),0,1);
+    let day = newYear.getDay() - dowOffset; //the day of week the year begins on
     day = (day >= 0 ? day : day + 7);
     var daynum = Math.floor((that.getTime() - newYear.getTime() - 
-    (that.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
-    var weeknum;
+    (that.getTimezoneOffset() - newYear.getTimezoneOffset())*60000)/86400000) + 1;
+    let weeknum;
     //if the year starts before the middle of a week
     if(day < 4) {
-      weeknum = Math.floor((daynum+day-1)/7) + 1;
+      weeknum = Math.floor((daynum + day - 1)/7) + 1;
       if(weeknum > 52) {
-        nYear = new Date(that.getFullYear() + 1,0,1);
-        nday = nYear.getDay() - dowOffset;
+        let nYear = new Date(that.getFullYear() + 1,0,1);
+        let nday = nYear.getDay() - dowOffset;
         nday = nday >= 0 ? nday : nday + 7;
         /*if the next year starts before the middle of
           the week, it is week #1 of that year*/
@@ -134,7 +136,7 @@ ml.date={};
       }
     }
     else {
-      weeknum = Math.floor((daynum+day-1)/7);
+      weeknum = Math.floor((daynum + day - 1)/7);
     }
     return weeknum;
   //};
