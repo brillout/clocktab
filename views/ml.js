882671b43d6c2143168590719ad281ad6837ec8b
Romuald Brillout <github.com@brillout.com>
2020-04-08 12:23:57 +0200

    move all commons code to tab-utils - wip

--- views/ml.js
+++ views/ml.js
@@ -1291,44 +1291,6 @@ ml.deleteCookies=function()
 //}}}
 */
 
-//offline stuff
-(function() {
-  var head;
-  ml.loadASAP=function(url,onsuccess){
-    head = head || document.getElementsByTagName('head')[0];
-
-    var attempts = 0;
-    function do_(){
-      //using same script el => onerror doesn't get called again in chrome
-      var script= document.createElement('script');
-          script.src= url;
-        //script.type= 'text/javascript';
-          script.onerror = function(){
-            head.removeChild(script);
-            setTimeout(do_,Math.min(Math.pow(2,attempts)*1000,60000));
-          };
-          //proxy redirects url -> false onsuccess
-          //-no way found to catch this
-          if(onsuccess) script.onload  = onsuccess;
-      attempts++;
-      head.appendChild(script);
-    }
-    do_();
-  };
-})();
-
-//* use loadASAP instead
-ml.loadScript=function(url,onload)
-//{{{
-{
-  const scriptEl = document.createElement('script');
-  scriptEl.src= url;
-  scriptEl.async = true;
-  if(onload) scriptEl.onload = onload;
-  document.getElementsByTagName('head')[0].appendChild(scriptEl);
-  return scriptEl;
-};
-//}}}
 /*
 ml.loadCss=function(url)
 //{{{
