070af4a0847dc69b0145541c2c80eb29b4e8a511
Romuald Brillout <github.com@brillout.com>
2020-04-23 19:41:06 +0200

    minor

--- tab_app_head_tags.js
+++ tab_app_head_tags.js
@@ -2,5 +2,5 @@ module.exports.tab_app_head_tags = [
   '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">',
   '<meta name="google-site-verification" content="ZWSwZhZ4dqhLnPNOXQvaWowDbsaD6aQiIu1h8O4_XUw" />',
   // Trial Token for Wake Lock API
-  '<meta http-equiv="origin-trial" content="AgxKaR4IN2JPaIsApBeQLg86YpAZktVd7xv93vh4BalRl7b7+Qfh0ItDlq3uRNp9VmF7mmnccJfdydglCcrR2QgAAABSeyJvcmlnaW4iOiJodHRwczovL3d3dy5jbG9ja3RhYi5jb206NDQzIiwiZmVhdHVyZSI6Ildha2VMb2NrIiwiZXhwaXJ5IjoxNTg3NTEzNTk5fQ==">',
+  '<meta http-equiv="origin-trial" content="AuBkDWSrveJGM5qjy8FIfMgCl7v4N+TjqRiviv1Smv6marflAUFaHLGAGsaZ2fxfR8WgoFqH5WeAUYrlUd8j7gkAAABSeyJvcmlnaW4iOiJodHRwczovL3d3dy5jbG9ja3RhYi5jb206NDQzIiwiZmVhdHVyZSI6Ildha2VMb2NrIiwiZXhwaXJ5IjoxNTkxMjkyNDI0fQ==">',
 ];
