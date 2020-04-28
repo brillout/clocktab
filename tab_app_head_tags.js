8e1353c2ac755e1689f6ff338a22b79e413fdcba
Romuald Brillout <github.com@brillout.com>
2020-04-28 11:17:09 +0200

    [code-removal] implement new ad code

--- tab_app_head_tags.js
+++ tab_app_head_tags.js
@@ -1,6 +1,5 @@
 module.exports.tab_app_head_tags = [
   '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">',
-  '<meta name="google-site-verification" content="ZWSwZhZ4dqhLnPNOXQvaWowDbsaD6aQiIu1h8O4_XUw" />',
   // Trial Token for Wake Lock API
   '<meta http-equiv="origin-trial" content="AuBkDWSrveJGM5qjy8FIfMgCl7v4N+TjqRiviv1Smv6marflAUFaHLGAGsaZ2fxfR8WgoFqH5WeAUYrlUd8j7gkAAABSeyJvcmlnaW4iOiJodHRwczovL3d3dy5jbG9ja3RhYi5jb206NDQzIiwiZmVhdHVyZSI6Ildha2VMb2NrIiwiZXhwaXJ5IjoxNTkxMjkyNDI0fQ==">',
 ];
---
+++ views/ClockPage/ad_slots.ts
@@ -0,0 +1,12 @@
+export { ad_slots };
+
+const ad_slots = [
+  {
+    slot_id: "8079958663",
+    slot_name: "ATF",
+  },
+  {
+    slot_id: "9556691865",
+    slot_name: "BTF",
+  },
+];
