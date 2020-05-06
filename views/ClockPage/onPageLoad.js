50666ec0498a1724772c9d494477c2deb7517915
Romuald Brillout <github.com@brillout.com>
2020-05-06 12:42:56 +0200

    update tab-utils

--- views/ClockPage/onPageLoad.js
+++ views/ClockPage/onPageLoad.js
@@ -4,7 +4,7 @@ import auto_remove_hash from "../../tab-utils/auto_remove_hash";
 import { on_big_text_load, set_max_width_getter } from "../BigText";
 import { sleep } from "../../tab-utils/sleep";
 import { ad_slots } from "./ad_slots";
-import { load_ad } from "../../tab-utils/load_ad";
+import { load_ads } from "../../tab-utils/load_ad";
 
 export default onPageLoad;
 
@@ -23,7 +23,7 @@ async function onPageLoad(load_common) {
 
   load_common();
 
-  load_ad(ad_slots);
+  load_ads(ad_slots);
 
   // To avoid memory leak
   //autoReloadPage();
