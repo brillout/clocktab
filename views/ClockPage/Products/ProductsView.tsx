0a24a51f0d366e191636260e5040fdea43986d85
Romuald Brillout <github.com@brillout.com>
2020-05-13 12:59:48 +0200

    minor

--- views/ClockPage/Products/ProductsView.tsx
+++ views/ClockPage/Products/ProductsView.tsx
@@ -17,17 +17,23 @@ function ProductsView() {
       <div id="product-list">
         <ProductView
           name="product-clock-fan"
-          text="USB Clock Fan &mdash; for hot summer days :-)."
+          text={
+            <span>
+              USB Clock Fan &mdash; for hot summer days :-).
+              <br />
+              <br />
+            </span>
+          }
           link="https://www.amazon.com/gp/product/B010M9KRHO/ref=as_li_ss_tl?ie=UTF8&linkCode=ll1&tag=brilloutamazo-20&linkId=37d16e9c32f7c039fca2b3730639d27e"
         />
         <ProductView
           name="product-tablet-monitor"
-          text="Tablet monitor, perfect to put Clock Tab on."
-          link="https://www.amazon.com/Portable-Monitor-Computer-1920%C3%971080-Protector/dp/B07RGPCQG1/ref=as_li_ss_tl?dchild=1&keywords=lepow&qid=1588966606&sr=8-1&linkCode=ll1&tag=brilloutamazo-20&linkId=f7c74ebde53387fe5b4e8bd97f5ba559"
+          text="Tablet monitor, perfect to put Clock Tab on! By a no-name brand but it's well designed."
+          link="https://www.amazon.com/UPERFECT-Portable-1920%C3%971080-Raspberry-Cellphone/dp/B07K8JLR4C/ref=as_li_ss_tl?dchild=1&keywords=tablet+monitor+12&qid=1589308273&refinements=p_72:2661618011&rnid=2661617011&sr=8-1&linkCode=ll1&tag=brilloutamazo-20&linkId=375eebbf83f4c1c3758e3975f8c58d05"
         />
         <ProductView
           name="product-vockgeng-clock"
-          text="Desk Clock by Vockgeng, with background."
+          text="Desk Clock by Vockgeng. Available with many different backgrounds."
           link="https://www.amazon.com/Vockgeng-Sunflower-Charging-Function-6-2x3-8x0-9/dp/B0875WBM2N/ref=as_li_ss_tl?dchild=1&keywords=Vockgeng+clock&qid=1589231158&sr=8-23&th=1&linkCode=ll1&tag=brilloutamazo-20&linkId=871192f4426176e2048523b3f9c06038"
         />
       </div>
@@ -58,7 +64,7 @@ function load_product_view() {
   `;
 
   document.querySelector("#product-tablet-monitor").innerHTML = `
-    <a href="https://www.amazon.com/Portable-Monitor-Computer-1920%C3%971080-Protector/dp/B07RGPCQG1/ref=as_li_ss_il?dchild=1&keywords=lepow&qid=1588966606&sr=8-1&linkCode=li3&tag=brilloutamazo-20&linkId=277061cf2fe45f339307d412e9264e10" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07RGPCQG1&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=brilloutamazo-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=brilloutamazo-20&l=li3&o=1&a=B07RGPCQG1" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+    <a href="https://www.amazon.com/UPERFECT-Portable-1920%C3%971080-Raspberry-Cellphone/dp/B07K8JLR4C/ref=as_li_ss_il?dchild=1&keywords=tablet+monitor+12&qid=1589308273&refinements=p_72:2661618011&rnid=2661617011&sr=8-1&linkCode=li3&tag=brilloutamazo-20&linkId=e8497f14d55d8da11c3cef37e1a85a9a" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07K8JLR4C&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=brilloutamazo-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=brilloutamazo-20&l=li3&o=1&a=B07K8JLR4C" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
   `;
   document.querySelector("#product-vockgeng-clock").innerHTML = `
     <a href="https://www.amazon.com/Vockgeng-Sunflower-Charging-Function-6-2x3-8x0-9/dp/B0875WBM2N/ref=as_li_ss_il?dchild=1&keywords=Vockgeng+clock&qid=1589231158&sr=8-23&th=1&linkCode=li3&tag=brilloutamazo-20&linkId=3923a336d01beeae2194af92741a399d" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0875WBM2N&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=brilloutamazo-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=brilloutamazo-20&l=li3&o=1&a=B0875WBM2N" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
